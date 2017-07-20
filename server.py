87c4cc1f46e92ef1c8a229a96b488f0dcbdaa9f9
qero <cwb.hu@siat.ac.cn>
2017-07-20 20:56:20 +0800

    add subprocess IO and form

--- server.py
+++ server.py
@@ -3,8 +3,9 @@
 #this file is going to start a flask server, that can translate some file into the shell and return output
 #via the API endpoint, in order to frond end web page to display.
 
-from flask import Flask
+from flask import Flask, redirect, url_for
 from flask import request
+import shlex
 import subprocess
 
 app = Flask(__name__)
@@ -14,20 +15,34 @@ def hello():
     #TODO: add a index with form to paste code/contract address
     return "Hello World!"
 
-@app.route("/sol", methods=['POST'])
+@app.route("/sol", methods=['GET', 'POST'])
 def upload_sol():
-    #upload sol and return results
+    #upload sol and return results, use subprocess Popen constructor and PIP stdout
     if not request.method == 'POST':
-        return 'Only post solidity file allowed'
-    result = {'reentry':False, 'concurrecy':False, 'timestamp':False, 'callstack':False}
-
-    return request.form['name']
+        return '''
+        <form method="post">
+            <p><textarea cols=40 rows=10 name=code style="background-color:BFCEDC"></textarea>
+            <p><input type=submit value=Verify>
+        </form>
+    '''
+    solfile = request.form['code']
+    with open('temp.sol', 'w') as tempfile:
+        tempfile.write(solfile)
+    solc_cmd = "solc --optimize --bin-runtime %s"
+    #result = {'reentry':False, 'concurrecy':False, 'timestamp':False, 'callstack':False}
+    res = subprocess.Popen(shlex.split(
+      solc_cmd % 'temp.sol'
+    ), stdout=subprocess.PIPE)
+    resout = res.communicate()
+    return resout
 
 
 @app.route('/address/<c_addr>')
 def c_address(c_addr):
     #download the contract code to perform a verification;
     #fan write crawler
-    pass
+
+    #pass
+    return c_addr
 
 
