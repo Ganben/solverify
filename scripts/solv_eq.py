da3708737f446d28dddf8765edb94ae74ff1c95e
Ganben <caozhao@gmail.com>
2017-09-14 23:57:15 +0800

    new problem added

--- scripts/solv_eq.py
+++ scripts/solv_eq.py
@@ -11,3 +11,30 @@ s.add(2*x - 2*y + 4*z == -2)
 s.add(-x + 0.5*y - z == 0)
 print s.check()
 print s.model()
+
+
+
+circle, square, triangle = Ints('circle square triangle')
+s = Solver()
+s.add(circle+circle==10)
+s.add(circle*square+square==12)
+s.add(circle*square-triangle*circle==circle)
+print s.check()
+print s.model()
+
+out = BitVec('out', 64)
+tmp=[]
+for i in range(64):
+    tmp.append((out>>i)&0x3F)
+s=Solver()
+# all overlapping 6-bit chunks must be distinct:
+s.add(Distinct(*tmp))
+# MSB must be zero:
+s.add((out&0x8000000000000000)==0)
+print s.check()
+result=s.model()[out].as_long()
+print "0x%x" % result
+# print overlapping 6-bit chunks:
+for i in range(64):
+    t=(result>>i)&0x3F
+    print " "*(63-i) + format(t, 'b').zfill(6)
